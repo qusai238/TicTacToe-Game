3aa50fcd34464e54fa2e8f5ce848498196626eca
Qusai Al-Buqaeen <74375388+qusai238@users.noreply.github.com>
2023-10-12 16:10:51 +0100

    Create game.py

---
+++ game.py
@@ -0,0 +1,61 @@
+from board import Board
+from player import Player
+
+
+class TicTacToeGame:
+
+    def start(self):
+        print("**************************")
+        print("  Welcome to Tic-Tac-Toe  ")
+        print("**************************")
+
+        board = Board()
+        player = Player()
+        computer = Player(False)
+
+        board.print_board()
+
+        while True:
+
+            while True:
+
+                player_move = player.get_move()
+                board.submit_move(player, player_move)
+                board.print_board()
+
+                if board.check_is_tie():
+                    print("It's a tie! 👍 Try again.")
+                    break
+                elif board.check_is_game_over(player, player_move):
+                    print("Awesome. You won the game! 🎉")
+                    break
+                else:
+                    computer_move = computer.get_move()
+                    board.submit_move(computer, computer_move)
+                    board.print_board()
+
+                    if board.check_is_game_over(computer, computer_move):
+                        print("Oops... 😱 The Computer Won. Try again.")
+                        break
+
+            play_again = input("\nWould you like to play again? Enter X for YES or O for NO: ").upper()
+
+            if play_again == "O":
+                print("\nBye! Come back soon 👋")
+                break
+            elif play_again == "X":
+                self.start_new_round(board)
+            else:
+                print("Your input was not valid but I will assume that you want to play again. 💡")
+                self.start_new_round(board)
+
+    def start_new_round(self, board):
+        print("*************")
+        print("  New Round  ")
+        print("*************")
+        board.reset_board()
+        board.print_board()
+
+
+game = TicTacToeGame()
+game.start()
